a69b55f08e1afa23172a40f8f2870793f828f7f3
amirbadbakht312-cpu <amirbadbakht312@gmail.com>
2026-06-11 06:47:21 +0330

    Create words.py

---
+++ words.py
@@ -0,0 +1,11 @@
+WORDS = {
+    "A1": [
+        {"en": "apple", "fa": "سیب"},
+        {"en": "book", "fa": "کتاب"},
+        {"en": "water", "fa": "آب"}
+    ],
+    "A2": [
+        {"en": "important", "fa": "مهم"},
+        {"en": "future", "fa": "آینده"}
+    ]
+}
